572f46e620edf3d3a8b8b2783d2156b08ebbc3e0
Holly Murphy <h_murphy@Hollys-MacBook-Pro.local>
2017-06-05 22:00:35 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state={
+    name:'',
+    repos:[]
+  }
+
+  getRepos = () =>{
+    axios.get('https://api.github.com/users/ho-mu/repos')
+      .then((response)=>{
+        console.log(`in then`)
+        this.setState((prev)=>{
+          let newState = { prev };
+          
+          response.data.forEach((val)=>{
+            this.state.repos.push(val.full_name)
+          })
+          
+          return newState
+        })
+        
+      })
+  }
+
+
   render() {
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" placeholder='Git UserName'  value='ho-mu' />
+        <button type='submit' onClick={this.getRepos} >Get Repos</button>
+        <hr />
+        <ul><DisplayRepos repos={this.state.repos}/></ul>
       </div>
     );
   }
+}
+
+const DisplayRepos = (props) => {
+  console.log(`props: `, props)
+  return(
+     <li> {props.repos} </li>
+  )
 }
 
 export default App;
